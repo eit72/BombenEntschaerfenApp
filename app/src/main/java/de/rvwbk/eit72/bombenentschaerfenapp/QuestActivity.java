d10e1f12e78fc3ed50d3802b3d6c055fa71fe62f
shikku <christianbuchheister@hotmail.com>
2019-06-04 08:36:41 +0200

    1

--- app/src/main/java/de/rvwbk/eit72/bombenentschaerfenapp/QuestActivity.java
+++ app/src/main/java/de/rvwbk/eit72/bombenentschaerfenapp/QuestActivity.java
@@ -6,27 +6,28 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class QuestActivity extends AppCompatActivity {
+    List<Button> buttons = new ArrayList<Button>();
+    TextView question;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView tvQuestion;
-        Button btnAnswer1;
-        Button btnAnswer2;
-        Button btnAnswer3;
-        Button btnAnswer4;
+        question = findViewById(R.id.tv_question_id);
+        Button answer1 = findViewById(R.id.btn_answer_1_id);
+        Button answer2 = findViewById(R.id.btn_answer_2_id);
+        Button answer3 = findViewById(R.id.btn_answer_3_id);
+        Button answer4 = findViewById(R.id.btn_answer_4_id);
 
-        tvQuestion = findViewById(R.id.tv_question_id);
-
-        btnAnswer1 = findViewById(R.id.btn_answer_1_id);
-        btnAnswer2 = findViewById(R.id.btn_answer_2_id);
-        btnAnswer3 = findViewById(R.id.btn_answer_3_id);
-        btnAnswer4 = findViewById(R.id.btn_answer_4_id);
+        buttons.add(answer1);
+        buttons.add(answer2);
+        buttons.add(answer3);
+        buttons.add(answer4);
 
 
     }
@@ -34,14 +35,16 @@ public class QuestActivity extends AppCompatActivity {
     public QuestActivity(){};
 
     public QuestActivity (String quest,String correctAnswer,List<String> answers){
-        List<Button> ansButtons =  null;
 
-        for(Button b: ansButtons){
-            b.setOnClickListener(new View.OnClickListener() {
+
+        for(Button button: buttons){
+            button.setText(quest);
+
+            button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
 
-                };
+                }
             });
         }
     }

07c16f4b7b744d637ed100b30d941f7edf40c49e
Anton <mrmendo@hotmail.de>
2019-06-24 09:33:21 +0200

    TestHelper for the Database

--- app/src/main/java/de/rvwbk/eit72/bombenentschaerfenapp/MainActivity.java
+++ app/src/main/java/de/rvwbk/eit72/bombenentschaerfenapp/MainActivity.java
@@ -2,48 +2,32 @@ package de.rvwbk.eit72.bombenentschaerfenapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
 
-import java.util.UUID;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
-import de.rvwbk.eit72.bombenentschaerfenapp.beacon.BeaconHandlerCallback;
-import de.rvwbk.eit72.bombenentschaerfenapp.beacon.BeaconHandler;
+import de.rvwbk.eit72.bombenentschaerfenapp.database.DatabaseHelper;
 
 public class MainActivity extends AppCompatActivity {
-    private BeaconHandler handler;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        handler = new BeaconHandler(getApplicationContext(), getUUID(), new BeaconHandlerCallback() {
-            @Override
-            public void OnEnter(int index) {
-                Toast.makeText(getApplicationContext(), "Entered: " + String.valueOf(index), Toast.LENGTH_SHORT).show();
-            }
+        List<HashMap<String, Object>> documents = new ArrayList<>();
+        HashMap<String, Object> quest1 = new HashMap<>();
+        HashMap<String, Object> quest2 = new HashMap<>();
 
-            @Override
-            public void OnExit(int index) {
-                Toast.makeText(getApplicationContext(), "Exited: " + String.valueOf(index), Toast.LENGTH_SHORT).show();
-            }
+        quest1.put("title", "Die erste Quest");
+        quest1.put("description", "Die erste Quest Beschreibung");
+        quest2.put("title", "Die zweite Quest");
+        quest2.put("description", "Die zweite Quest Beschreibung");
 
-            @Override
-            public void OnConnected(){
-                handler.listenToAll();
-            }
-        });
-    }
+        documents.add(quest1);
+        documents.add(quest2);
 
-    private UUID getUUID(){
-        String str = getString(R.string.beacon_UUID);
-
-        return UUID.fromString(str);
-    }
-
-    @Override
-    protected void onDestroy() {
-        handler.onDestroy();
-        super.onDestroy();
+        DatabaseHelper.insertCollection("dummyQuests", documents);
+//        System.out.println("HEY: " + DatabaseHelper.getCollectionDocuments("users").toString());
     }
 }

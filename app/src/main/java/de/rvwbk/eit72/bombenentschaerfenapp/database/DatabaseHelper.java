4e417afff1154dfed715062ded48f23b89f67f8d
Anton <mrmendo@hotmail.de>
2019-06-24 09:24:07 +0200

    TestHelper for the Database

--- app/src/main/java/de/rvwbk/eit72/bombenentschaerfenapp/database/DatabaseHelper.java
+++ app/src/main/java/de/rvwbk/eit72/bombenentschaerfenapp/database/DatabaseHelper.java
@@ -4,12 +4,19 @@ import android.support.annotation.NonNull;
 import android.util.Log;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 
 public class DatabaseHelper {
     private static FirebaseFirestore DB = FirebaseFirestore.getInstance();
@@ -18,7 +25,8 @@ public class DatabaseHelper {
         final HashMap<String, Object> documents = new HashMap<>();
         documents.put("hey", "ho");
 
-        DB.collection(collectionName).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+        Task task = DB.collection(collectionName).get();
+        task.addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 if (task.isSuccessful()) {
@@ -33,5 +41,25 @@ public class DatabaseHelper {
         });
 
         return documents;
+    }
+//DB.collection(collectionName)
+//    .add(user)
+//        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+//    @Override
+//    public void onSuccess(DocumentReference documentReference) {
+//      Log.d("cTAG", "DocumentSnapshot added with ID: " + documentReference.getId());
+//    }
+//  })
+//    .addOnFailureListener(new OnFailureListener() {
+//    @Override
+//    public void onFailure(@NonNull Exception e) {
+//      Log.w("cTAG", "Error adding document", e);
+//    }
+//  });
+    public static void insertCollection(String collectionName, List<HashMap<String, Object>> documents){
+      Iterator it = documents.iterator();
+      while(it.hasNext()){
+        DB.collection(collectionName).add(it.next());
+      }
     }
 }

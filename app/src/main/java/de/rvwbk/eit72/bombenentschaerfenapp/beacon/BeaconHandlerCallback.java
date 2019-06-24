9fe215518f236bb6f9a9d8558b27a7b11c55a82d
Anton <mrmendo@hotmail.de>
2019-06-24 09:34:33 +0200

    TestHelper for the Database

--- app/src/main/java/de/rvwbk/eit72/bombenentschaerfenapp/beacon/BeaconHandlerCallback.java
+++
@@ -1,7 +0,0 @@
-package de.rvwbk.eit72.bombenentschaerfenapp.beacon;
-
-public interface BeaconHandlerCallback {
-    void OnEnter(int index);
-    void OnExit(int index);
-    void OnConnected();
-}
